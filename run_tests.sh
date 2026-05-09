0709936217bd4e40f364ae3a8f7dc1d703e10644
Liam Powell <liam@liampwll.com>
2026-05-10 07:28:40 +0800

    Switch to gnatcov 26 from Alire

--- run_tests.sh
+++ run_tests.sh
@@ -7,14 +7,16 @@ set -e
 
 cd tests
 
+[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -- gnatcov setup --prefix=./gnatcov_rts
+
 # The test suite needs a lot of cleaning up before validation works
 # [ "$PRUNT_SKIP_VALIDATION" = true ] || alr build --validation
 
 # Instrument the project code for coverage analysis.
-[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- /usr/local/gnat/bin/gnatcov instrument --level=stmt+mcdc+gexpr --dump-trigger=manual --projects=prunt.gpr --projects=tests.gpr --no-subprojects --ada=2022
+[ "$PRUNT_DO_GNATCOV" = true ] && GPR_PROJECT_PATH="$GPR_PROJECT_PATH:./gnatcov_rts/share/gpr" alr exec -P2 -- gnatcov instrument --level=stmt+mcdc+gexpr --dump-trigger=manual --projects=prunt.gpr --projects=tests.gpr --no-subprojects --ada=2022
 
 # Build testsuite with instrumented code.
-[ "$PRUNT_DO_GNATCOV" = true ] && alr build --development -- --src-subdirs=gnatcov-instr --implicit-with=gnatcov_rts
+[ "$PRUNT_DO_GNATCOV" = true ] && GPR_PROJECT_PATH="$GPR_PROJECT_PATH:./gnatcov_rts/share/gpr" alr build --development -- --src-subdirs=gnatcov-instr --implicit-with=gnatcov_rts.gpr
 [ "$PRUNT_DO_GNATCOV" = true ] || alr build --development
 
 rm -f gnatcov_out/*.xcov
@@ -28,11 +30,11 @@ rm -f *.srctrace
 
 # Run the GNATcov code coverage analysis on the trace files.
 # tests.gpr can be added below to check that all tests are being run.
-[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- /usr/local/gnat/bin/gnatcov coverage --annotate=report --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
-[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- /usr/local/gnat/bin/gnatcov coverage --annotate=shtml+ --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
-[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- /usr/local/gnat/bin/gnatcov coverage --annotate=xcov+ --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
-# alr exec -P2 -- /usr/local/gnat/bin/gnatcov coverage --annotate=sarif --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
-# alr exec -P2 -- /usr/local/gnat/bin/gnatcov coverage --annotate=cobertura --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
+[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- gnatcov coverage --annotate=report --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
+[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- gnatcov coverage --annotate=shtml+ --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
+[ "$PRUNT_DO_GNATCOV" = true ] && alr exec -P2 -- gnatcov coverage --annotate=xcov+ --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
+# alr exec -P2 -- gnatcov coverage --annotate=sarif --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
+# alr exec -P2 -- gnatcov coverage --annotate=cobertura --output-dir=gnatcov_out --level=stmt+mcdc+gexpr --projects=prunt.gpr --no-subprojects *.srctrace
 
 [ "$PRUNT_XCOV_DUMP" = true ] && alr exec -P2 -- python3 generate_coverage.py
 
